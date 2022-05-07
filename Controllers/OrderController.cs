ba3945fb319f0a931390bc66d74b4b591e959b1f
JerryYang0730 <jacksonjong96@gmail.com>
2022-05-07 15:49:40 +0800

    Update

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -2,11 +2,34 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using MicroBusiness.Models;
 
+using System.Data.SqlClient;
+using Dapper;
+
 namespace Order.Controllers;
 
 public class OrderController : Controller{
-    public IActionResult Index(){
-        return View("Index");
+
+    private readonly string connectionString = "Server=localhost;Database=OrderDB;User ID=sa;Password=Pa$$w0rd;";
+
+    
+    
+    // static IList<Customer> customerList = new List<Customer>();
+    public IActionResult Index(string searchString){
+        using(var connection = new SqlConnection(connectionString)){
+            connection.Open();
+
+            string commandSQL = "Select * from Customer";
+
+            var customer = connection.Query<Customer>(commandSQL);
+
+            if(!String.IsNullOrEmpty(searchString)){
+                customer = customer.Where(c => c.FirstName.Contains(searchString) || c.LastName.Contains(searchString));    
+            }
+            connection.Close();
+            
+            return View(customer.ToList());
+        }
     }
+
 
 }
